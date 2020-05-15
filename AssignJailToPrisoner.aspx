41b0d69762d9fd6662a85fed6b8c7215d7001a98
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-05-15 07:12:42 +0500

    Assign Jail to Prisoner Feature uploaded

---
+++ AssignJailToPrisoner.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.Master" AutoEventWireup="true" CodeBehind="AssignJailToPrisoner.aspx.cs" Inherits="PrisonManagementSystem.AssignJailToPrisoner" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+            <div class="form-horizontal">
+                <h2>Assign Prison To Prisoner</h2>
+                <hr />
+                <h4>Please enter ID of Jailor, We wil assign jail to jailor </h4>
+                <div class="form-group">
+                    <asp:Label ID="lblID" runat="server" CssClass="col-md-2 control-label" Text="Enter PrisonerID"></asp:Label>
+                    <div class="col-md-3">
+                        <asp:TextBox ID="txt_id" CssClass="form-control" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidatorEmail" CssClass="text-danger" runat="server" ErrorMessage="Please enter your ID !" ControlToValidate="txt_id"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label ID="Label1" runat="server" CssClass="col-md-2 control-label" Text="Please Select a Jail"></asp:Label>
+                    <div class="col-md-3">
+                        <asp:DropDownList ID="txtJail" runat="server" CssClass="form-control" Width="100%">
+                            <asp:ListItem>Lahore</asp:ListItem>
+                            <asp:ListItem>Islamabad</asp:ListItem>
+                            <asp:ListItem>Karachi</asp:ListItem>
+                            <asp:ListItem>Peshawar</asp:ListItem>
+                            <asp:ListItem>Quetta</asp:ListItem>
+                        </asp:DropDownList>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" CssClass="text-danger" runat="server" ErrorMessage="Please Select a Jail" ControlToValidate="txtJail"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-6">
+                        <asp:Button ID="btn_Assign" runat="server" CssClass="btn btn-default" Text="Assign" OnClick="btn_Assign_Click"  />
+                        <asp:Label ID="lblPassRec" runat="server" CssClass="text-danger"></asp:Label>
+                    </div>
+                </div>
+            </div>
+     </div>
+</asp:Content>
---
+++ AssignJailToPrisoner.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace PrisonManagementSystem
+{
+    public partial class AssignJailToPrisoner : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=ABDULWAHAB;Initial Catalog=PrisonManagementSystem;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn.Open();
+        }
+
+        protected void btn_Assign_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd1 = new SqlCommand("select prisonerID as prisonerID from AssignJailToPrisoner where prisonerID= @id", conn);
+            cmd1.Parameters.AddWithValue("@id", txt_id.Text);
+            SqlDataReader dr;
+            dr = cmd1.ExecuteReader();
+            int count = 0;
+            while (dr.Read())
+            {
+                count += 1;
+            }
+            dr.Close();
+            if (count >= 1)
+            {
+                lblPassRec.Text = "Prisoner Already has assigned A jail";
+            }
+            else
+            {
+                checkJailorAndAssign();
+
+            }
+        }
+        public void checkJailorAndAssign()
+        {
+            string command = "INSERT INTO AssignJailToPrisoner(prisonerID,date,jailName) VALUES(@prisonerID,@date,@jailName)";
+            SqlCommand cmd = new SqlCommand(command, conn);
+            cmd.Parameters.AddWithValue("@prisonerID", txt_id.Text);
+            cmd.Parameters.AddWithValue("@jailName", txtJail.Text);
+            DateTime a = DateTime.Now;
+            DateTime b = a.Date;
+            cmd.Parameters.AddWithValue("@date", b);
+            SqlCommand cmd1 = new SqlCommand("select PrisonerID as PrisonerID from PrisonerTable where PrisonerID= @id", conn);
+            cmd1.Parameters.AddWithValue("@id", txt_id.Text);
+            SqlDataReader dr;
+            dr = cmd1.ExecuteReader();
+            int count = 0;
+            while (dr.Read())
+            {
+                count += 1;
+            }
+            dr.Close();
+            if (count > 0)
+            {
+
+                cmd.ExecuteNonQuery();
+                lblPassRec.Text = "Prisoner Assigned Successfully";
+            }
+            else
+            {
+                lblPassRec.Text = "Prisoner Not Found ";
+            }
+        }
+    }
+}
